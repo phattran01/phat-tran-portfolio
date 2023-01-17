7cbcba1ea531bf3a1140d05da44d7a1a3128279d
Phat Tran <71514299+phattran01@users.noreply.github.com>
2023-01-17 14:06:44 -0500

    First push - simple HTML, CSS, JS setup

---
+++ main.js
@@ -0,0 +1,25 @@
+function automaticText() {
+    $(document).ready(function() {
+        var initialText = "Hello! my name is Phat Tran and welcome to my portfolio.";
+        $("#automaticText-container").text(initialText);
+        var text = "";
+        var i = 0;
+        var delay = 100; // delay in milliseconds
+        
+        function type() {
+            if (i < initialText.length) {
+                text += initialText[i];
+                if(initialText[i] === "!") {
+                    text += "<br>";
+                }
+                if(i === 27) {
+                    text += "<br>";
+                }
+                $("#automaticText-container").html(text);
+                i++;
+                setTimeout(type, delay);
+            }
+        }
+        type();
+    });
+}
